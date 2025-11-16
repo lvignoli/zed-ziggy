1738002f5c1615ded5fe3839d59aca57095ff4f8
Louis Vignoli <louis.vignoli@gmail.com>
2025-11-16 22:09:49 +0100

    Update ziggy schema: brackets, outline and extension

--- languages/ziggy-schema/outline.scm
+++ languages/ziggy-schema/outline.scm
@@ -1,5 +1,3 @@
-(schema ("root" "=" root: (expr (identifier) @name)) @item )
+(schema ("$" "=" root: (expr (identifier) @name)) @item )
 (struct name: (_) @name) @item
 (struct_field key: (identifier) @name) @item
-(union name: (_) @name) @item
-(union_field key: (identifier) @name) @item
