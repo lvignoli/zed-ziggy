b76838a332b805bf8ac2e95713cfb79eea0af9b4
Louis Vignoli <louis.vignoli@gmail.com>
2025-11-16 22:09:49 +0100

    Update ziggy: brackets, outline and extension

--- languages/ziggy/outline.scm
+++ languages/ziggy/outline.scm
@@ -1,5 +1,2 @@
-(struct name: (_) @name) @item
-(struct_field key: (_) @name) @item
-
-(dict name : (_)@name) @item
-(dict_field key : (_)@name) @item
+(struct_field key: (identifier) @name) @item
+(dict_field key: (_) @name) @item
